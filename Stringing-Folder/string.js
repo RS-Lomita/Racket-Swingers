c9103bd2cfd6feb815de0c510bb3ebb077cd2da7
Pizzasamrad <Overload54101@gmail.com>
2025-03-02 23:49:25 -0800

    images

---
+++ Stringing-Folder/string.js
@@ -0,0 +1,11 @@
+function bookStringing() {
+    alert('🎾 Booking feature coming soon! Stay tuned.');
+}
+function sendInquiry() {
+    let email = document.getElementById('email').value;
+    if (email) {
+        alert('📩 Inquiry sent! We will contact you soon.');
+    } else {
+        alert('❌ Please enter a valid email address.');
+    }
+}
